f17418905650e7e64eb6f53ebdd7b12db41fb6db
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-14 13:37:52 +0300

    Создал компонент tickets-list

---
+++ src/components/tickets-list/tickets-list.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TicketListItem from "../ticketListItem";
+import classes from './ticketList.module.scss';
+const {ul}  = classes;
+
+export default function TicketList() {
+  return (
+    <ul className={ul}>
+      <TicketListItem />
+    </ul>
+  );
+}
