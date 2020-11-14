fbb9f197f158a56ffe3af8e3e9ef67a6e027a83f
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-14 13:49:46 +0300

    Собираюсь улучшить ф.filterOnLength

--- src/components/tickets-list/tickets-list.js
+++ src/components/tickets-list/tickets-list.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TicketListItem from "../ticketListItem";
+import TicketListItem from "../tickets-list-item/tickets-list-item.js";
 import classes from './ticketList.module.scss';
 const {ul}  = classes;
 
