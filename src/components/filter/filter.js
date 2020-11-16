5fb9e4c7d1955d9b67013220b96e5b5c400ff5d7
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-17 00:02:55 +0300

    Убрал лишний код с компонента filter

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,16 +1,7 @@
 import React from 'react'
 import { Button } from 'antd'
-import { Checkbox } from 'antd'
 import TabPanel from '../tab-panel/tab-panel'
-import { useDispatch, useSelector } from 'react-redux'
-import {
-	ALL,
-	NO_STOPS,
-	ONE_STOP,
-	THREE_STOPS,
-	TWO_STOPS,
-} from '../../constants'
-import filters from '../../action/filters'
+import { useDispatch} from 'react-redux'
 import { loadTickets } from '../../action/loadTickets'
 import Loader from '../loader/loader'
 import { counter } from '../../action/counter'
@@ -18,19 +9,10 @@ import Checkboxes from '../checkboxes/checkboxes'
 
 export default function Filter() {
 	const dispatch = useDispatch()
-	const loadedTickets = useSelector((store) => store.loadTickets.tickets)
-	// const checkboxes = [ALL, NO_STOPS, ONE_STOP, TWO_STOPS, THREE_STOPS].map(
-	// 	(el, i) => {
-	// 		return (
-	// 			<Checkbox key={i} onChange={() => dispatch(filters(el, loadedTickets))}>
-	// 				{el}
-	// 			</Checkbox>
-	// 		)
-	// 	}
-	// )
+
 	return (
 		<>
-			{/* {checkboxes} */}
+
             <Checkboxes />
 			<TabPanel />
 			<Button
