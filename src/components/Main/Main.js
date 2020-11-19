700fd86818bb0f54fc9791ef5ce3c742a101f176
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-19 21:23:02 +0300

    Add component Logo to component Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Filter from '../filter/filter';
+import Logo from '../logo/logo';
 
 export default function Main () {
     return <>
+    <Logo />
     <Filter />
     </>
 }
