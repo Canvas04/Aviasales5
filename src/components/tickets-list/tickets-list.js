63117afb0f55f0db7d4409c8b9fac9ce9b877ad9
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-19 22:30:18 +0300

    Add element Button which load tickets

--- src/components/tickets-list/tickets-list.js
+++ src/components/tickets-list/tickets-list.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { counter } from '../../action/counter.js'
+import { loadTickets } from '../../action/loadTickets.js'
 import TicketListItem from '../tickets-list-item/tickets-list-item.js'
 import './tickets-list.scss'
 
 export default function TicketList() {
+    const isLoading = useSelector(store => store.loadTickets.isFetching)
+    const dispatch = useDispatch()
 	return (
+        <>
 		<ul className="ul">
 			<TicketListItem />
 		</ul>
+        { !isLoading && <button type="button" class="btn btn-primary btn-md btn-block" onClick={() => {
+					dispatch(loadTickets())
+					dispatch(counter())
+				}}>Показать ещё</button>}
+        </>
 	)
 }
