be6f00b99220b905bf5d6fb7315d525502f2ebef
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-21 10:36:50 +0300

    Change load tabs

--- src/components/tab-panel/tab-panel.js
+++ src/components/tab-panel/tab-panel.js
@@ -3,14 +3,19 @@ import { Tabs } from 'antd'
 import Tab1 from '../../containers/tab1/tab1'
 import Tab2 from '../../containers/tab2/tab2'
 import './tab-panel.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import filters from '../../action/filters'
+import { CHEAPEST, FASTEST } from '../../constants'
 
 const { TabPane } = Tabs
 
 export default function TabPanel() {
+    const dispatch = useDispatch();
+    const tickets = useSelector(store => store.loadTickets.tickets)
 	return (
 		<>
-			<Tabs defaultActiveKey="1" type="card" size={'large'}>
-				<TabPane tab="Самый дешевый" key="1">
+			<Tabs defaultActiveKey="1" type="card" size={'large'} onChange={() => dispatch(filters(FASTEST,tickets))} >
+				<TabPane tab="Самый дешевый" key="1" >
 					<Tab1 />
 				</TabPane>
 				<TabPane tab="Caмый быстрый" key="2">
