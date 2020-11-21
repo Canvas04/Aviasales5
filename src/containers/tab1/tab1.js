c985d16c633d8dd50f5299c9cda8a3480d90418e
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-21 13:27:35 +0300

    Nothing changes

--- src/containers/tab1/tab1.js
+++ src/containers/tab1/tab1.js
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import filters from '../../action/filters';
 import TicketList from '../../components/tickets-list/tickets-list';
-import { FASTEST } from '../../constants';
+import { CHEAPEST } from '../../constants';
 
 export default function Tab1 () {
 const dispatch = useDispatch();
 const tickets = useSelector(store => store.loadTickets.tickets);
 
 useEffect(() => {
-    dispatch(filters(FASTEST,tickets))
+dispatch(filters(CHEAPEST,tickets))
 })
 return <>
 <TicketList />
--- src/containers/tab2/tab2.js
+++ src/containers/tab2/tab2.js
@@ -2,13 +2,14 @@ import React,{useEffect} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
 import filters from '../../action/filters';
 import TicketList from '../../components/tickets-list/tickets-list';
-import { CHEAPEST } from '../../constants';
+import {   FASTEST } from '../../constants';
 
 export default function Tab2 () {
     const dispatch = useDispatch();
     const tickets = useSelector(store => store.loadTickets.tickets)
     useEffect(() => {
-        dispatch(filters(CHEAPEST,tickets))
+        dispatch(filters(FASTEST,tickets))
+
     })
 return <>
 <TicketList />
