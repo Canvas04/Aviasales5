03599278827e68453d9b59b2b50b02567e85f034
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-21 10:17:04 +0300

    Change names of tabs

--- src/components/tab-panel/tab-panel.js
+++ src/components/tab-panel/tab-panel.js
@@ -10,10 +10,10 @@ export default function TabPanel() {
 	return (
 		<>
 			<Tabs defaultActiveKey="1" type="card" size={'large'}>
-				<TabPane tab="Самый быстрый" key="1">
+				<TabPane tab="Самый дешевый" key="1">
 					<Tab1 />
 				</TabPane>
-				<TabPane tab="Caмый дешевый" key="2">
+				<TabPane tab="Caмый быстрый" key="2">
 					<Tab2 />
 				</TabPane>
 			</Tabs>
