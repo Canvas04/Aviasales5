b4861587b666ac8ee5702f61fe75d2871ca180f2
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-22 19:08:02 +0300

    Remove useless spaces

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,12 +1,24 @@
-import { ALL, CHEAPEST, FASTEST, FILTER, NO_STOPS, THREE_STOPS, TWO_STOPS ,ONE_STOP} from '../constants'
-import { filterOnLabel,  filterOnPrice, filterOnSpeed ,filterOnLength} from '../secondaryFunc'
-
+import {
+	ALL,
+	CHEAPEST,
+	FASTEST,
+	FILTER,
+	NO_STOPS,
+	THREE_STOPS,
+	TWO_STOPS,
+	ONE_STOP,
+} from '../constants'
+import {
+	filterOnLabel,
+	filterOnPrice,
+	filterOnSpeed,
+	filterOnLength,
+} from '../secondaryFunc'
 
 const filters = (state = null, action) => {
-
 	switch (action.type) {
 		case FILTER:
-			return filterOnLength(getTickets(action),action.counter)
+			return filterOnLength(getTickets(action), action.counter)
 
 		default:
 			return state
@@ -16,22 +28,21 @@ export default filters
 
 const getTickets = (filter) => {
 	switch (filter.nameFilter) {
-
 		case FASTEST:
 			return filterOnSpeed(filter.payload)
 		case CHEAPEST:
-            return filterOnPrice(filter.payload)
-            case ALL:
-                return filterOnLabel(filter.payload,filter.nameFilter)
-            case NO_STOPS:
-                return filterOnLabel(filter.payload,filter.nameFilter)
+			return filterOnPrice(filter.payload)
+		case ALL:
+			return filterOnLabel(filter.payload, filter.nameFilter)
+		case NO_STOPS:
+			return filterOnLabel(filter.payload, filter.nameFilter)
 
-            case ONE_STOP:
-                return filterOnLabel(filter.payload,filter.nameFilter)
-            case TWO_STOPS:
-                return filterOnLabel(filter.payload,filter.nameFilter)
-            case THREE_STOPS:
-            return filterOnLabel(filter.payload,filter.nameFilter)
+		case ONE_STOP:
+			return filterOnLabel(filter.payload, filter.nameFilter)
+		case TWO_STOPS:
+			return filterOnLabel(filter.payload, filter.nameFilter)
+		case THREE_STOPS:
+			return filterOnLabel(filter.payload, filter.nameFilter)
 
 		default:
 			return filter.payload
