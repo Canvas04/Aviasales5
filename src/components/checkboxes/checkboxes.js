dd95139f2f3b6fa20a789e4933146c2262daec37
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-22 21:08:29 +0300

    Add component GroupCheck

--- src/components/checkboxes/checkboxes.js
+++ src/components/checkboxes/checkboxes.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Checkbox } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCheckId, getCheckLabel, getCheck } from '../../secondaryFunc/index'
@@ -9,25 +9,24 @@ import './checkboxes.scss'
 export default function Checkboxes() {
 	const dispatch = useDispatch()
 	const stateCheckboxes = useSelector((store) => store.checkboxes.items)
-    const loadedTickets = useSelector((store) => store.loadTickets.tickets)
-    const storeCheckboxes = useSelector(store => store.checkboxes)
+	const loadedTickets = useSelector((store) => store.loadTickets.tickets)
+	const storeCheckboxes = useSelector((store) => store.checkboxes)
 	const elements = stateCheckboxes.map((el) => {
 		return (
 			<Checkbox
 				key={el.id}
-				onChange={() =>{
-                    dispatch(
+				onChange={() => {
+					dispatch(
 						pressCheck(
 							getCheckId(el.id, stateCheckboxes),
-                            getCheckLabel(el.id, stateCheckboxes),
-                            storeCheckboxes
+							getCheckLabel(el.id, stateCheckboxes),
+							storeCheckboxes
 						)
-                    )
-                    dispatch( filters(getCheckLabel(el.id, stateCheckboxes),loadedTickets))
-                }
-
-
-				}
+					)
+					dispatch(
+						filters(getCheckLabel(el.id, stateCheckboxes), loadedTickets)
+					)
+				}}
 				checked={getCheck(el.id, stateCheckboxes)}
 			>
 				{getCheckLabel(el.id, stateCheckboxes)}
@@ -35,4 +34,32 @@ export default function Checkboxes() {
 		)
 	})
 	return <>{elements}</>
+}
+
+const plainOptions = [
+	'Без пересадок',
+	'1 пересадка',
+	'2 пересадки',
+	'3 пересадки',
+]
+const defaultCheckedList = []
+const CheckboxGroup = Checkbox.Group
+
+export function GroupCheck() {
+    const [checkedList,setCheckedList] = useState(defaultCheckedList)
+    const [checkAll,setCheckAll] = useState(false)
+    const onChange = list => {
+        setCheckedList(list)
+        setCheckAll(list.length === plainOptions.length)
+    }
+    const onCheckAllChange = e => {
+        setCheckedList(e.target.checked ? plainOptions : [])
+        setCheckAll(e.target.checked)
+    }
+	return <>
+    <Checkbox onChange={onCheckAllChange} checked={checkAll} >
+        Все
+    </Checkbox>
+    <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
+    </>
 }
