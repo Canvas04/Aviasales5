14f820647abab8ef1deb6bd75b0f8acfd6fb01ff
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-22 22:45:52 +0300

    Remove from combineReducers reducer checkboxes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import loadTickets from './loadTickets'
 import filters from './filters'
-import checkboxes from './checkboxes';
 import {counter} from './counter'
 
-export default combineReducers({loadTickets,filters,checkboxes,counter});
+export default combineReducers({loadTickets,filters,counter});
