14af8a18100906527b92d0805ffeddfff3ea8f7c
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-24 00:01:56 +0300

    Change names classes for jsx elements

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,33 +1,27 @@
 import React from 'react'
 import { Button } from 'antd'
 import TabPanel from '../tab-panel/tab-panel'
-import { useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { loadTickets } from '../../action/loadTickets'
 import { counter } from '../../action/counter'
 import Checkboxes, { GroupCheck } from '../checkboxes/checkboxes'
-import  './filter.scss'
-
-
+import './filter.scss'
+import { divideOnRanges } from '../../secondaryFunc'
 
 export default function Filter() {
 	const dispatch = useDispatch()
 
 	return (
-        <>
-		<div className='filter'>
-<aside className='aside'>
-<h1 className='header'>КОЛИЧЕСТВО ПЕРЕСАДОК</h1>
- <GroupCheck />
-</aside>
-<aside className='search'>
-
-
-			<TabPanel />
-
-</aside>
-		</div>
-
-
-            </>
+		<>
+			<div className="filter">
+				<div className="filter__checkboxes">
+					<h1 className="filter__header">КОЛИЧЕСТВО ПЕРЕСАДОК</h1>
+					<GroupCheck />
+				</div>
+				<div className="filter__tab-panel">
+					<TabPanel />
+				</div>
+			</div>
+		</>
 	)
 }
