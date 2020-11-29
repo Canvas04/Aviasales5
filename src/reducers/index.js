2a0fdba982906fc876f1730dda6f06ac08dbcbb5
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-29 14:32:40 +0300

    Add reducer check on func combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux'
 import loadTickets from './loadTickets'
 import filters from './filters'
-import {counter} from './counter'
+import { counter } from './counter'
+import check from './checkboxes'
 
-export default combineReducers({loadTickets,filters,counter});
+export default combineReducers({ loadTickets, filters, counter, check })
