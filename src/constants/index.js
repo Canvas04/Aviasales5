fb699f0c5d93b90ab3fd55029665d8a40e0e0582
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-29 14:03:23 +0300

    Add new object with Action

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,21 +6,25 @@ export const urlConstants = {
 	TICKETS_URL: 'https://front-test.beta.aviasales.ru/tickets?searchId',
 }
 export const actionsForLoading = {
-    FETCH_REQUEST_TICKETS: 'FETCH_REQUEST_TICKETS',
-    FETCH_RECEIVE_TICKETS : 'FETCH_RECEIVE_TICKETS',
-    FETCH_FAILURE_TICKETS: 'FETCH_FAILURE_TICKETS'
+	FETCH_REQUEST_TICKETS: 'FETCH_REQUEST_TICKETS',
+	FETCH_RECEIVE_TICKETS: 'FETCH_RECEIVE_TICKETS',
+	FETCH_FAILURE_TICKETS: 'FETCH_FAILURE_TICKETS',
 }
 export const actionsForFilters = {
-    FILTER: 'FILTER',
-    FASTEST: 'FASTEST',
-    CHEAPEST: 'CHEAPEST',
-    ALL: 'Все',
-    NO_STOPS: 'Без пересадок',
-    ONE_STOP: '1 пересадка',
-    TWO_STOPS: '2 пересадки',
-    THREE_STOPS: '3 пересадки',
-    DEPARTURE: 'DEPARTURE',
-    ARRIVAL: 'ARRIVAL'
+	FILTER: 'FILTER',
+	FASTEST: 'FASTEST',
+	CHEAPEST: 'CHEAPEST',
+	ALL: 'Все',
+	NO_STOPS: 'Без пересадок',
+	ONE_STOP: '1 пересадка',
+	TWO_STOPS: '2 пересадки',
+	THREE_STOPS: '3 пересадки',
+	DEPARTURE: 'DEPARTURE',
+	ARRIVAL: 'ARRIVAL',
 }
 
 export const COUNTER = 'COUNTER'
+
+export const actionForCheckboxes = {
+	PRESS_CHECK: 'PRESS_CHECK',
+}
