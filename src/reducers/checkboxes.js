cb6b5ccadb027cb4630cbfb1d8d827bc76841771
Askarov Ruslan <askarow.rus@yandex.ru>
2020-11-29 14:30:54 +0300

    Add reducer for checkboxes

---
+++ src/reducers/checkboxes.js
@@ -0,0 +1,43 @@
+import { actionForCheckboxes } from '../constants/index'
+import { controlChecks } from '../secondaryFunc'
+const { PRESS_CHECK } = actionForCheckboxes
+
+const initialState = {
+	items: [
+		{
+			id: 1,
+			label: 'Все',
+			checked: false,
+		},
+		{
+			id: 2,
+			label: 'Без пересадок',
+			checked: false,
+		},
+		{
+			id: 3,
+			label: '1 пересадка',
+			checked: false,
+		},
+		{
+			id: 4,
+			label: '2 пересадки',
+			checked: false,
+		},
+		{
+			id: 5,
+			label: '3 пересадки',
+			checked: false,
+		},
+	],
+	selectAll: false,
+}
+
+export default function check(state = initialState, action) {
+	switch (action.type) {
+		case PRESS_CHECK:
+			return controlChecks(state.items, action.id, state.selectAll)
+		default:
+			return state
+	}
+}
