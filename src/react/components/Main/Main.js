43343da99232ae8b2ff99a58d71bf6fdce7c078f
ruslan.aslarov <ruslan.askarov@osinit.com>
2020-12-03 09:35:42 -0800

    Remove component Logo

--- src/react/components/Main/Main.js
+++ src/react/components/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Filter from '../filter/filter'
-import Logo from '../logo/logo'
+import './Main.scss'
+import logo from './Logo.svg'
+
 export default function Main() {
 	return (
 		<>
-			<Logo />
+		<div className='logo-block'>
+				<img src={logo} alt="logo" width={60} height={60} />
+			</div>
 			<Filter />
 		</>
 	)
