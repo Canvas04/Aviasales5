1fb7c46663808eb03b4849d5a150a3e953880c9c
Askarov Ruslan <askarow.rus@yandex.ru>
2020-12-05 11:54:58 +0300

    Add new reducer in rootReducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux'
 import loadTickets from './tickets/tickets'
 import filters from './filters/filters'
 import check from './checkboxes/checkboxes'
+import defineTab from './defineActiveTab/defineActiveTab'
 
-export default combineReducers({ loadTickets, filters, check })
+export default combineReducers({ loadTickets, filters, check, defineTab })
