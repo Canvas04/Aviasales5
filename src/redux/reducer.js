587f8f09544ef02dcdd00b24cecaa27e840fbb33
ruslan.aslarov <ruslan.askarov@osinit.com>
2020-12-10 06:20:41 -0800

    Add new reducer currentFilter

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,5 +3,6 @@ import loadTickets from './tickets/tickets'
 import filters from './filters/filters'
 import check from './checkboxes/checkboxes'
 import defineTab from './defineActiveTab/defineActiveTab'
+import currentFilter from './current-filter/current-filter'
 
-export default combineReducers({ loadTickets, filters, check, defineTab })
+export default combineReducers({ loadTickets, filters, check, defineTab ,currentFilter})
