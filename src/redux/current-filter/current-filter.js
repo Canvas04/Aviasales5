3ef32122c53ce52d281034f6f6d654c7480b7267
ruslan.aslarov <ruslan.askarov@osinit.com>
2020-12-10 06:29:33 -0800

    Add reducer currentFilter

---
+++ src/redux/current-filter/current-filter.js
@@ -0,0 +1,9 @@
+const currentFilter = (state = 'Все', action) => {
+	switch (action.type) {
+		case 'CURRENT':
+            return  action.filter
+		default:
+			return state
+	}
+}
+export default currentFilter
