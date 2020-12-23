c586bbe8dfe724f87dd59bf5c669e8846f9ac645
Ruslan04071996 <askarow.rus@yandex.ru>
2020-12-23 17:36:33 +0300

    Add component Button

---
+++ src/react/components/button/button.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd'
+import React from 'react'
+import styled from 'styled-components'
+
+export default () => {
+	return (
+		<>
+			<Button >Показать еще</Button>
+		</>
+	)
+}
